dd5ef007a994ae806e62863389fe2c43d6a8a316
Ajay Shrestha <32661119+shrestha-ajay@users.noreply.github.com>
2022-03-14 01:01:23 -0400

    Initial

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express')
+const app = express();
+
+const port = 8080;
+const host = '0.0.0.0';
+
+app.get('/', (req, res) => {
+  res.send('Congratulations on your Node Hello World Application built on Docker!');
+})
+
+app.listen(port, host);
+console.log(`Running on http://${host}:${port}`);
